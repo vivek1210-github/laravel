b583a09a928cc6a3b77c386e539576c3dac499d0
vivek1210-github <vivek.12mails@gmail.com>
2020-12-19 23:39:30 +0530

    Bitfumes study

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+
+    public function uploadAvatar(Request $request) {
+
+        if($request->hasFile('image')) {
+
+            $filename = $request->image->getClientOriginalName();
+
+            if(auth()->user()->avatar)
+            {
+                Storage::delete('/public/images/'.auth()->user()->avatar);
+            }
+
+          // $request->image->store('images', 'public');
+           $request->image->storeAs('images', $filename, 'public');
+           //$request->image->move('custom-images/', $filename);
+           // Auth::user()->update(['avatar' => $filename]);
+           auth()->user()->update(['avatar' => $filename]);
+          return redirect()->back();
+        }
+    }
+
+    public function index() {
+
+        // DB::insert('insert into users (name, email, password) values (?, ?, ?)',
+        //  ['sarthak', 'sarthak@test.com', 'password']);
+      //  $users = DB::select('select * from users');
+       //  DB::update('update users set name = ? where id = 1', ['John Smith']);
+    //    DB::delete('delete from users where id = ?', [1]);
+    //     $users = DB::select('select * from users');
+    //     return $users;
+
+        // $user  = new User();
+        // $user->name = "vivek";
+        // $user->email = 'vivek@test.com';
+        // $user->password = bcrypt('password');
+        // $user->save();
+
+       // User::where('id', 2)->delete();
+      // User::where('id', 3)->update(['name' => 'vivekgupta']);
+
+      $data = [
+          'name' => 'Sarthak',
+          'email' => 'sarthak@test.com',
+          'password' => 'password'
+      ];
+
+     // User::create($data);
+
+        $user = User::all();
+       // dd($user);
+        return $user;
+
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return config('services.ses.key');
+    return view('welcome');
+});
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::post('/upload', [UserController::class, 'uploadAvatar']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
